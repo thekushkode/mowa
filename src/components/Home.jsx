0e01368407815a3fbafa3b99df37f53b993e8e8e
Robert L. Kushner IV <robertkushner4@gmail.com>
2020-09-05 13:18:02 -0400

    add streak, etc

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import {
     MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBMask,
-    MDBRow, MDBCol, MDBIcon, MDBBtn, MDBView, MDBContainer
+    MDBRow, MDBCol, MDBIcon, MDBBtn, MDBView, MDBContainer, MDBStreak
 } from "mdbreact";
 import "./Home.css";
 import Nav from "./Nav";
@@ -10,6 +10,8 @@ import videoMp4 from '../assets/elder1.mp4';
 import videoOgg from '../assets/elder1.ogg';
 import videoWebm from '../assets/elder1.webm';
 import Footer from "./Footer";
+import streakpic from '../assets/oldladyandwoman.jpg';
+import oldguy from '../assets/oldguy.jpg';
 
 class VideoBackgroundPage extends React.Component {
     state = {
@@ -57,22 +59,26 @@ class VideoBackgroundPage extends React.Component {
                             </MDBContainer>
                         </MDBMask>
                     </MDBView>
+                    <div>
+                        <MDBRow className='mt-4 mx-4'>
+                            <MDBCol md='6' lg='6' >
+                                <img style={{ width: '80%' }} src={oldguy} alt='old man eating soup' />
+                            </MDBCol>
+                            <MDBCol className='column' md='6' lg='6' >
+                                <h4>
+                                    Meals On Wheels Atlanta (MOWA) was founded in 1965 to support seniors struggling with poverty, food insecurity, and social isolation. Over more than 50 years, we have grown from a small soup kitchen to an organization serving more than 514,000 meals each year throughout the Atlanta area. We have also expanded our program offerings to respond to other critical needs in our community. We now offer multiple services to support both older adults and veterans, including home-delivered and congregate meals, home repair services, and a food pantry.
+                                </h4>
 
-                    <MDBContainer>
-                        <MDBRow className="pt-5 pb-4">
-                            <MDBCol md="12" className="text-center">
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                                    enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                                    in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                                    nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                                    sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
                             </MDBCol>
                         </MDBRow>
-                    </MDBContainer>
+                    </div>
+
+                    <div style={{ marginTop: '20px' }}>
+                        <MDBStreak className='w-auto' size="sm" by="MOW ATL" overlayClass="white-text rgba-black-strong" photo={streakpic}>
+                            Our mission is to support senior independence through meals, shelter, education, and community.
+                        </MDBStreak>
+                    </div>
+
                 </div>
                 <footer>
                     <Footer />
