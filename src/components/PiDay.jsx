bd38148be1a35664ab5330502850c1d1e87252b3
Robert L. Kushner IV <robertkushner4@gmail.com>
2021-03-30 12:25:03 -0400

    truffles

--- src/components/PiDay.jsx
+++ src/components/PiDay.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
 import './MOWAColors.css';
-import pi from '../assets/piday.png';
+import pi from '../assets/truffles.png';
 
 class PiDay extends Component {
     state = {
@@ -20,12 +20,12 @@ class PiDay extends Component {
             <MDBContainer>
                 <MDBBtn color="primary" onClick={this.toggle(14)} hidden>MDBModal</MDBBtn>
                 <MDBModal isOpen={this.state.modal14} toggle={this.toggle(14)} centered>
-                    <MDBModalHeader toggle={this.toggle(14)}>Celebrate Pi Day With Us!</MDBModalHeader>
+                    <MDBModalHeader toggle={this.toggle(14)}>Delicious Spring Truffles!</MDBModalHeader>
                     <MDBModalBody>
-                        <a href='https://secure.givelively.org/event/meals-on-wheels-atlanta/pi-day' target='_blank' rel="noopener noreferrer"><img src={pi} className='img-fluid' alt='piday promotion' /></a>
+                        <a href='https://secure.givelively.org/event/meals-on-wheels-atlanta/gourmet-spring-truffles?blm_aid=40295425' target='_blank' rel="noopener noreferrer"><img src={pi} className='img-fluid' alt='piday promotion' /></a>
                     </MDBModalBody>
                     <MDBModalFooter>
-                        <MDBBtn href='https://secure.givelively.org/event/meals-on-wheels-atlanta/pi-day' target='_blank' rel="noopener noreferrer" className='mowablue'>Place Order</MDBBtn>
+                        <MDBBtn href='https://secure.givelively.org/event/meals-on-wheels-atlanta/gourmet-spring-truffles?blm_aid=40295425' target='_blank' rel="noopener noreferrer" className='mowablue'>Place Order</MDBBtn>
                         <MDBBtn className='mowaorange' onClick={this.toggle(14)}>Close</MDBBtn>
                     </MDBModalFooter>
                 </MDBModal>
