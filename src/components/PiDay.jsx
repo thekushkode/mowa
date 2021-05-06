413c33e423f45f9144454976d6f9b7e8047cc4db
Robert L. Kushner IV <robertkushner4@gmail.com>
2021-05-06 15:59:24 -0400

    jovita.2

--- src/components/PiDay.jsx
+++ src/components/PiDay.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
 import './MOWAColors.css';
-import pi from '../assets/mommy.png';
+import pi from '../assets/jov.png';
 
 class PiDay extends Component {
     state = {
@@ -21,14 +21,14 @@ class PiDay extends Component {
             <MDBContainer>
                 <MDBBtn color="primary" onClick={this.toggle(14)} hidden>MOWA</MDBBtn>
                 <MDBModal isOpen={this.state.modal14} toggle={this.toggle(14)} centered size='lg'>
-                    <MDBModalHeader toggle={this.toggle(14)}>Plan the Perfect Mother's Day!</MDBModalHeader>
+                    <MDBModalHeader toggle={this.toggle(14)}>Help Support Jovita Moore!</MDBModalHeader>
                     <MDBModalBody>
-                        <a href='https://www.purposefulgourmetfoods.org/collections/chefs-kitchen/products/chefs-kitchen' target='_blank' ><img src={pi} className='img-fluid' alt='Mothers Day Meal Poster' /></a>
+                        <Link to='/donate' ><img src={pi} className='img-fluid' alt='Mothers Day Meal Poster' /></Link>
                     </MDBModalBody>
                     <MDBModalFooter>
-                        {/* <Link to='/donate' > */}
-                        <MDBBtn href='https://www.purposefulgourmetfoods.org/collections/chefs-kitchen/products/chefs-kitchen' className='mowablue'>Order Now</MDBBtn>
-                        {/* </Link> */}
+                        <Link to='/donate' >
+                        <MDBBtn className='mowablue'>Donate Now</MDBBtn>
+                        </Link>
                         <MDBBtn className='mowaorange' onClick={this.toggle(14)}>Close</MDBBtn>
                     </MDBModalFooter>
                 </MDBModal>
