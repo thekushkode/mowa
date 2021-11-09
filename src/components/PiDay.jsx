56f3b25892699a755773e048503512dcf24c0e4b
Robert L. Kushner IV <robertkushner4@gmail.com>
2021-11-09 15:11:23 -0500

    turkey day offering

--- src/components/PiDay.jsx
+++ src/components/PiDay.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
 import './MOWAColors.css';
-import pi from '../assets/Jovita.png';
+import pi from '../assets/turkey.png';
 
 class PiDay extends Component {
     state = {
@@ -21,17 +21,17 @@ class PiDay extends Component {
             <MDBContainer>
                 <MDBBtn color="primary" onClick={this.toggle(14)} hidden>MOWA</MDBBtn>
                 <MDBModal isOpen={this.state.modal14} toggle={this.toggle(14)} centered size='lg'>
-                    {/* <MDBModalHeader toggle={this.toggle(14)}>In Loving Memory</MDBModalHeader> */}
-                    {/* <MDBModalBody>
-                        <a href='https://e.givesmart.com/events/ns7/' target='_blank' rel="noopener noreferrer" ><img src={pi} className='img-fluid' alt='Golf Poster' /></a>
-                    </MDBModalBody> */}
+                    <MDBModalHeader toggle={this.toggle(14)}>Turkey Day Made Easy!</MDBModalHeader>
                     <MDBModalBody>
-                        <img src={pi} className='img-fluid' alt='In memory of Jovita Moore' />
+                        <a href='https://www.purposefulgourmetfoods.org/products/thanksgiving-offering' target='_blank' rel="noopener noreferrer" ><img src={pi} className='img-fluid' alt='Turkey Poster' /></a>
                     </MDBModalBody>
+                    {/* <MDBModalBody>
+                        <img src={pi} className='img-fluid' alt='Turkey Day Meals' />
+                    </MDBModalBody> */}
                     <MDBModalFooter>
-                        {/* <a href='https://secure.givelively.org/event/meals-on-wheels-atlanta/21st-annual-meals-on-wheels-atlanta-golf-classic' target='_blank' rel="noopener noreferrer" > */}
-                        {/* <MDBBtn href='https://e.givesmart.com/events/ns7/' target='_blank' rel="noopener noreferrer" className='mowablue'>Get Tickets</MDBBtn> */}
-                        {/* </a> */}
+                        <a href='https://www.purposefulgourmetfoods.org/products/thanksgiving-offering' target='_blank' rel="noopener noreferrer" >
+                        <MDBBtn href='https://www.purposefulgourmetfoods.org/products/thanksgiving-offering' target='_blank' rel="noopener noreferrer" className='mowablue'>Order Now</MDBBtn>
+                        </a>
                         <MDBBtn className='mowaorange' onClick={this.toggle(14)}>Close</MDBBtn>
                     </MDBModalFooter>
                 </MDBModal>
